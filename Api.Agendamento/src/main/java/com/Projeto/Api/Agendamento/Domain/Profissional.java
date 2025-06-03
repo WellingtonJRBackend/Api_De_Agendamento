cf14238b75504240c3a01c1da8c157e4cc0d0e85
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2025-06-02 22:10:13 -0300

    Realizando anotações nas entidades

--- Api.Agendamento/src/main/java/com/Projeto/Api/Agendamento/Domain/Profissional.java
+++ Api.Agendamento/src/main/java/com/Projeto/Api/Agendamento/Domain/Profissional.java
@@ -1,11 +1,17 @@
 package com.Projeto.Api.Agendamento.Domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profissional {
 
     @Id
