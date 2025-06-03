d1e1f89ff361d54663a56ee70addc70a1a771c02
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2025-06-02 22:00:33 -0300

    Criação da entidade serviço

--- Api.Agendamento/src/main/java/com/Projeto/Api/Agendamento/Domain/Servico.java
+++ Api.Agendamento/src/main/java/com/Projeto/Api/Agendamento/Domain/Servico.java
@@ -1,25 +1,18 @@
 package com.Projeto.Api.Agendamento.Domain;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-public class Cliente {
+public class Servico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
     private String nome;
-    private String email;
-    private String telefone;
-    private String senha;
+    private Double preco;
+    private Integer duracaoMin;
 }
